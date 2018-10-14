5946cb7b37392a61aed0a83c4a30ecc1f3e4537a
islamjaidul <jaidul26@gmail.com>
2018-10-14 13:59:21 +0000

    database config has been changed

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/MosqueApp');
+// mongoose.connect('mongodb://localhost:27017/MosqueApp');
+mongoose.connect('mongodb://islamjaidul:Ind38maK@ds045464.mlab.com:45464/mosque_app');
 
 module.exports = {mongoose};
